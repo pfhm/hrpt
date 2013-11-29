f13a26f362de4797423457857057a932f852769b
Mattias Wallenberg <mattias.wallenberg@tieto.com>
2013-11-29 10:07:57 +0100

    Fixed an issue where FutureFill rules weren't working on textareas.

--- media/pollster/wok/js/wok.pollster.rules.js
+++ media/pollster/wok/js/wok.pollster.rules.js
@@ -69,6 +69,10 @@
                         if (this.value == value)
                             this.selected = true;
                     });
+                    break;
+                case "textarea":
+                	this.value = value;
+                	break;
                 break;
             }
         });
