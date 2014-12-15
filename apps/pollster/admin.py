dd9e89ebea46e4123b246b3d2e75644d81a3ff22
Mattias Wallenberg <mattias.wallenberg@tieto.com>
2014-12-15 14:22:02 +0100

    New bulk operation for deactivating users (with proper admin changelogging)

--- apps/pollster/admin.py
+++ apps/pollster/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin 
+from django.contrib.auth.models import User
+from django.contrib.admin.models import LogEntry, CHANGE
+from django.contrib.contenttypes.models import ContentType
 
 from .models import RuleType, QuestionDataType, VirtualOptionType, Survey, TranslationSurvey, Chart
 
@@ -8,4 +12,30 @@ admin.site.register(VirtualOptionType)
 admin.site.register(Survey)
 admin.site.register(TranslationSurvey)
 admin.site.register(Chart)
+
+
+current_user_admin = type(admin.site._registry[User])
+class ExtendedUserAdmin(current_user_admin):
+    actions = ['batch_deactivate'] 
+
+    def batch_deactivate(self, request, queryset): 
+        queryset.update(is_active = False)
+        
+        ct = ContentType.objects.get_for_model(queryset.model)
+        for obj in queryset:
+            LogEntry.objects.log_action(
+                user_id = request.user.id,
+                content_type_id = ct.pk,
+                object_id = obj.pk,
+                object_repr = obj.username,
+                action_flag = CHANGE,
+                change_message = 'User deactivated as part of a batch operation.')
+         
+        pass
+    
+    batch_deactivate.short_description = 'Deactivate selected users'
+
+admin.site.unregister(User)
+admin.site.register(User, ExtendedUserAdmin)
+
 
