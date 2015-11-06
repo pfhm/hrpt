fbec2d248f6ef3533d638e8af7e4250f97516c96
Joe FHM developer <developer@fhm>
2015-11-06 17:54:24 +0100

    added comment explaining the motivation of the last migration

--- db/migrations/003_sync_pollster_translationoption_seq.sql
+++ db/migrations/003_sync_pollster_translationoption_seq.sql
@@ -1,1 +1,5 @@
+-- This specific sequence was out of sync after an install.
+-- Possibly others have the same problem, so it might be a good idea tu run a
+-- script that fixes them all.
+
 SELECT SETVAL('pollster_translationoption_id_seq', (SELECT MAX(id) + 1  FROM pollster_translationoption));
