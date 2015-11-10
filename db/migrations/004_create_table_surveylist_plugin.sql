c52c0eaaf5f001d7f18ff314caa9bb5e374d4a12
Joe FHM developer <developer@fhm>
2015-11-10 18:48:06 +0100

    added migration for cmsplugin survey list

---
+++ db/migrations/004_create_table_surveylist_plugin.sql
@@ -0,0 +1,10 @@
+-- Creates the table cmsplugin_surveylistplugin. This is not going to hold any data
+-- but it must exist for the survey list cms plugin to work.
+
+CREATE TABLE cmsplugin_surveylistplugin
+(
+  cmsplugin_ptr_id integer
+)
+WITH (
+  OIDS=FALSE
+);
