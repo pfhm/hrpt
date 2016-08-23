02d9545ccf48f222f5f3dfd481fa83c1b854dde5
pfhm <admin@fohm.se>
2016-08-23 13:49:51 +0200

    Added user_profile (birthyear and activationcode) in the admin interface. Updated translations.

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
+#For the new user_profile admin
+from models import user_profile
 
 from .forms import UnicodeUserChangeForm, UnicodeUserCreationForm
 
+#new
+class UserProfileInline(admin.StackedInline):
+    model = user_profile
+
 current_user_admin = type(admin.site._registry[User])
+
 class UnicodeUserAdmin(current_user_admin):
     form = UnicodeUserChangeForm
     add_form = UnicodeUserCreationForm
+    inlines = [ UserProfileInline, ]
+
+
+
 
 admin.site.unregister(User)
 admin.site.register(User, UnicodeUserAdmin)
-
-
